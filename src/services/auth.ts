0cecff9ad7f5e70ec9f6d0a9c5d903b81ae01e81
Jettsyy <teazyyy2512@gmail.com>
2025-02-17 11:06:22 +0700

    feat:login form

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,8 +1,18 @@
+import { LoginSchemaType } from "../components/auth/Login";
 import { RegisterType } from "../components/auth/Register"
 import instanceAxios from "../ultis/configAxios"
 
-const login = () => {
-
+interface ILoginResponse {
+    accessToken: string,
+    user: RegisterType
+}
+const login = async (user: LoginSchemaType) => {
+    try {
+        const { data } = await instanceAxios.post<ILoginResponse>('/login', user);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const register = async (user: RegisterType) => {
